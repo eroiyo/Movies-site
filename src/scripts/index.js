026212c1fcd372b1922c0653a0fdd6f463a614de
Erez Friemagor <erezfree29@gmail.com>
2021-08-05 05:43:58 +0100

    add your score and refresh score

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,28 +2,70 @@ import '../style.css';
 
 const fetch = require('node-fetch');
 
-fetch("https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/", {
-      
+fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/', {
+
+  // Adding method type
+  method: 'POST',
+
+  // Adding body or contents to send
+  body: JSON.stringify({
+    name: 'My cool game',
+  }),
+
+  // Adding headers to the request
+  headers: {
+    'Content-type': 'application/json; charset=UTF-8',
+  },
+})
+
+  .then((response) => response.json())
+  .then((json) => console.log(json));
+
+document.querySelector('.align').addEventListener('click', () => {
+  const guser = document.querySelector('.gname').value;
+  const gscore = document.querySelector('.gscore').value;
+
+  fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/7BvSyhf1KA0jncm7oXBX/scores', {
+
     // Adding method type
-    method: "POST",
-      
+    method: 'POST',
+
     // Adding body or contents to send
     body: JSON.stringify({
-        name: "my new amazing game",
+      user: guser,
+      score: gscore,
     }),
-      
+
     // Adding headers to the request
     headers: {
-        "Content-type": "application/json; charset=UTF-8"
-    }
-})
-  
-// Converting to JSON
-.then(response => response.json())
-  
-// Displaying results to console
-.then(json => console.log(json));
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+  })
 
-
-document.querySelector('.submit').addEventListener('click', () => {
+    .then((response) => response.json())
+    .then((json) => console.log(json));
+  document.querySelector('.gname').value = '';
+  document.querySelector('.gscore').value = '';
+  event.preventDefault();
 });
+
+document.querySelector('.refresh').addEventListener('click', () => {
+  fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/7BvSyhf1KA0jncm7oXBX/scores')
+    .then((response) => response.json())
+    .then((data) => fillTable(data));
+});
+
+function fillTable(data) {
+  const highestUser = document.querySelector('.highest');
+  const highestScore = document.querySelector('.highest_score');
+  highestUser.textContent = Object.values(data)[0][0].user;
+  highestScore.textContent = Object.values(data)[0][0].score;
+  const shighestUser = document.querySelector('.shighest');
+  const shighestScore = document.querySelector('.shighest_score');
+  shighestUser.textContent = Object.values(data)[0][1].user;
+  shighestScore.textContent = Object.values(data)[0][1].score;
+  const lhighestUser = document.querySelector('.lhighest');
+  const lhighestScore = document.querySelector('.lhighest_score');
+  lhighestUser.textContent = Object.values(data)[0][2].user;
+  lhighestScore.textContent = Object.values(data)[0][2].score;
+}
