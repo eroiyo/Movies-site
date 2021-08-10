d69f1db006c82ee8ff007fa5d07a6a3ab0a0b831
eroiyo <59938389+eroiyo@users.noreply.github.com>
2021-08-10 10:02:39 -0400

    Add dynamic cards

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,5 +1,9 @@
 /* eslint-disable no-console */
+import { result } from 'lodash';
 import '../style.css';
+import { spawnCards } from './spawn-cards';
+
+const target = document.querySelector('.card-container')
 
 // eslint-disable-next-line no-unused-vars
 const fetch = require('node-fetch');
@@ -12,4 +16,6 @@ async function fetchResultsJSON() {
 
 fetchResultsJSON().then((results) => {
   console.log(results); // fetched movies
+  results.splice(8, 1);
+  spawnCards(results, target);
 });
