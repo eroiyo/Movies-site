838581950862339c5c368e1bd4a41cb0add099af
Erez Friemagor <erezfree29@gmail.com>
2021-08-10 12:12:45 +0100

    fetch all comedies

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -5,12 +5,11 @@ import '../style.css';
 const fetch = require('node-fetch');
 
 async function fetchResultsJSON() {
-    const response = await fetch('https://api.tvmaze.com/search/shows?q=comedy#');
-    const results = await response.json();
-    return results;
-  }
-  
-  fetchResultsJSON().then((results) => {
-    console.log(results); // fetched movies
-  });
-  
+  const response = await fetch('https://api.tvmaze.com/search/shows?q=comedy#');
+  const results = await response.json();
+  return results;
+}
+
+fetchResultsJSON().then((results) => {
+  console.log(results); // fetched movies
+});
