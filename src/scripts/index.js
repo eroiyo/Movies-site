6f569cd21cc82ce69faacb44cfd1e14527693364
eroiyo <59938389+eroiyo@users.noreply.github.com>
2021-08-10 14:00:46 -0400

    add appcreation function

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -4,6 +4,21 @@ import '../style.css';
 // eslint-disable-next-line no-unused-vars
 const fetch = require('node-fetch');
 
+const Appcreation  = () => {
+  const request = new XMLHttpRequest();
+  const requestURL = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/';
+  const params = "";
+  request.open('POST', requestURL, true);
+  request.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+  request.onreadystatechange = () => {
+    if (request.readyState === 4 && request.status === 201) {
+      alert(request.responseText);
+    }
+  };
+  request.send();
+};
+
+
 async function fetchResultsJSON() {
   const response = await fetch('https://api.tvmaze.com/search/shows?q=comedy#');
   const results = await response.json();
