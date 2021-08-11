1201490ba223f0ff7f1372d05b1f30978b13c239
Erez Friemagor <erezfree29@gmail.com>
2021-08-11 16:23:22 +0100

    show comments for show

--- src/scripts/spawn-cards.js
+++ src/scripts/spawn-cards.js
@@ -1,41 +1,36 @@
 import { dropRight, result } from "lodash";
 
 export const spawnCard = (movie, target) => {
-
-const fetchComments = () => { // the curly brace opens a multiline function
+  const fetchComments = (itemId) => { // the curly brace opens a multiline function
     async function fetchResultsJSON() {
-      const response = await fetch('https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/Q6FJ5Iv0xZsu9v3INtJx/comments?item_id=Inside%20Comedy');
+      itemId = itemId.replace(/\s/g, '%20');
+      const api = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/Q6FJ5Iv0xZsu9v3INtJx/comments?item_id=' + itemId
+      const response = await fetch(api);
       const results = await response.json();
       return results;
     }
 
     fetchResultsJSON().then((results) => { // fetched movies
-      results.splice(8, 1);
-      console.log(results);
       const commentsContent = document.querySelector('.comments_content');
-      for (let i = 0; i < results.length - 1; i += 1) {
+      while (commentsContent.firstChild) commentsContent.removeChild(commentsContent.firstChild);
+      for (let i = 0; i < results.length; i += 1) {
         const commentDiv = document.createElement('div');
-        
+        commentDiv.style.display = 'flex';
         const username = results[i].username;
         const date = results[i].creation_date;
         const content = results[i].comment;
-
-
         const userNameDiv = document.createElement('div');
+        userNameDiv.style.marginRight = '60px';
         const dateDiv = document.createElement('div');
+        dateDiv.style.marginRight = '60px';
         const contentDiv = document.createElement('div');
-        
         userNameDiv.textContent = username;
         dateDiv.textContent = date;
         contentDiv.textContent = content;
-
         commentDiv.appendChild(userNameDiv);
         commentDiv.appendChild(dateDiv);
         commentDiv.appendChild(contentDiv);
-        commentDiv.style.display = 'flex';
         commentsContent.appendChild(commentDiv);
-        userNameDiv.style.marginRight = '60px';
-        dateDiv.style.marginRight = '60px';
       }
     });
   };
@@ -72,7 +67,8 @@ const fetchComments = () => { // the curly brace opens a multiline function
     document.querySelector('.summary_text').innerHTML = movie.show.summary;
     document.querySelector('.comments_container').style.display = 'block';
     document.querySelector('main').style.height = '30%';
-    fetchComments();
+    const itemId = cardTitle.textContent;
+    fetchComments(itemId);
   });
 
   likeContainer.appendChild(likes);
