9a8d9cb9e8072e838ef5a5d9148afd522bf76602
eroiyo <59938389+eroiyo@users.noreply.github.com>
2021-08-11 07:04:21 -0400

    Fix like.js linter 1

--- src/scripts/likes.js
+++ src/scripts/likes.js
@@ -1,17 +1,16 @@
 const getLikes = async () => fetch('https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/Q6FJ5Iv0xZsu9v3INtJx/likes', {
-    method: 'GET',
-    // Adding headers to the request
-    headers: {
-      'Content-type': 'application/json; charset=UTF-8',
-    },
-  })
-    .then((response) => response.json())
-    .then((json) => (json),
-  );
+  method: 'GET',
+  // Adding headers to the request
+  headers: {
+    'Content-type': 'application/json; charset=UTF-8',
+  },
+})
+  .then((response) => response.json())
+  .then((json) => (json),);
 
 export const updateLikes = async () => {
   const array = await getLikes();
-  array.forEach(likes => {
+  array.forEach(likes () => {
     const target = document.getElementById(`like-${likes.item_id}`);
     target.textContent = `Likes: ${likes.likes}`
   });
