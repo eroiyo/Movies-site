35038d8fdc48a05d4cb9f9e03c2eb5a3bf4b7b57
Erez Friemagor <erezfree29@gmail.com>
2021-08-11 14:47:38 +0100

    show comments

--- src/scripts/spawn-cards.js
+++ src/scripts/spawn-cards.js
@@ -1,4 +1,45 @@
+import { dropRight, result } from "lodash";
+
 export const spawnCard = (movie, target) => {
+
+const fetchComments = () => { // the curly brace opens a multiline function
+    async function fetchResultsJSON() {
+      const response = await fetch('https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/Q6FJ5Iv0xZsu9v3INtJx/comments?item_id=Inside%20Comedy');
+      const results = await response.json();
+      return results;
+    }
+
+    fetchResultsJSON().then((results) => { // fetched movies
+      results.splice(8, 1);
+      console.log(results);
+      const commentsContent = document.querySelector('.comments_content');
+      for (let i = 0; i < results.length - 1; i += 1) {
+        const commentDiv = document.createElement('div');
+        
+        const username = results[i].username;
+        const date = results[i].creation_date;
+        const content = results[i].comment;
+
+
+        const userNameDiv = document.createElement('div');
+        const dateDiv = document.createElement('div');
+        const contentDiv = document.createElement('div');
+        
+        userNameDiv.textContent = username;
+        dateDiv.textContent = date;
+        contentDiv.textContent = content;
+
+        commentDiv.appendChild(userNameDiv);
+        commentDiv.appendChild(dateDiv);
+        commentDiv.appendChild(contentDiv);
+        commentDiv.style.display = 'flex';
+        commentsContent.appendChild(commentDiv);
+        userNameDiv.style.marginRight = '60px';
+        dateDiv.style.marginRight = '60px';
+      }
+    });
+  };
+
   const card = document.createElement('div');
   card.classList.add('card');
 
@@ -31,6 +72,7 @@ export const spawnCard = (movie, target) => {
     document.querySelector('.summary_text').innerHTML = movie.show.summary;
     document.querySelector('.comments_container').style.display = 'block';
     document.querySelector('main').style.height = '30%';
+    fetchComments();
   });
 
   likeContainer.appendChild(likes);
@@ -49,3 +91,11 @@ export const spawnCards = (array, target) => {
     spawnCard(movie, target);
   });
 };
+
+
+
+'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/Q6FJ5Iv0xZsu9v3INtJx/comments?item_id=Comedy%20Legends
+
+
+
+Comedy Legends
