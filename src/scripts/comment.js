1b4f573ed8a8edb953be6cac5d4a1770ee2bc389
Erez Friemagor <erezfree29@gmail.com>
2021-08-11 12:30:27 +0100

    apply post api

--- src/scripts/comment.js
+++ src/scripts/comment.js
@@ -1,23 +1,3 @@
+
 const fetch = require('node-fetch');
 
-fetch('https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/Q6FJ5Iv0xZsu9v3INtJx/comments', {
-
-  // Adding method type
-  method: 'POST',
-
-  // Adding body or contents to send
-  body: JSON.stringify({
-    item_id: 'Comedy',
-    username: 'Erez',
-    comment: 'One of the best show ever',
-  }),
-
-  // Adding headers to the request
-  headers: {
-    'Content-type': 'application/json; charset=UTF-8',
-  },
-})
-
-  .then((response) => response.json())
-  // eslint-disable-next-line no-console
-  .then((json) => console.log(json));
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -38,3 +38,37 @@ document.querySelector('.close').addEventListener('click', () => {
   document.querySelector('.card-container').style.display = 'grid';
   document.querySelector('main').style.height = '80%';
 });
+
+document.querySelector('.close').addEventListener('click', () => {
+  document.querySelector('.comments_container').style.display = 'none';
+  document.querySelector('.card-container').style.display = 'grid';
+  document.querySelector('main').style.height = '80%';
+});
+
+document.querySelector('.submit_button').addEventListener('click', () => {
+  const itemId = document.querySelector('.mname').textContent;
+  const userName = document.querySelector('.your_name').value;
+  const commentContent = document.querySelector('textarea').value;
+
+  fetch('https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/Q6FJ5Iv0xZsu9v3INtJx/comments', {
+
+    // Adding method type
+    method: 'POST',
+
+    // Adding body or contents to send
+    body: JSON.stringify({
+      item_id: itemId,
+      username: userName,
+      comment: commentContent,
+    }),
+
+    // Adding headers to the request
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+  })
+
+    .then((response) => response.json())
+  // eslint-disable-next-line no-console
+    .then((json) => console.log(json));
+});
