308f625693315d9250df076e67cae2b7e73410fd
eroiyo <59938389+eroiyo@users.noreply.github.com>
2021-08-11 06:49:42 -0400

    fix style linters

--- src/scripts/likes.js
+++ src/scripts/likes.js
@@ -1,39 +1,37 @@
 const getLikes = async () => {
- return fetch('https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/Q6FJ5Iv0xZsu9v3INtJx/likes', {
+  return fetch('https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/Q6FJ5Iv0xZsu9v3INtJx/likes', {
     method: 'GET',
     // Adding headers to the request
     headers: {
       'Content-type': 'application/json; charset=UTF-8',
     },
   })
-    .then((response) => response.json())
-    .then((json) => { return(json);
-        });
+  .then((response) => response.json())
+  .then((json) => {
+      return (json);
+  });
 }
 
 export const updateLikes = async () => {
-   const array = await getLikes();
-   array.forEach(likes => {
-       const target = document.getElementById(`like-${likes.item_id}`);
-       target.textContent=`Likes: ${likes.likes}`
-   });
+  const array = await getLikes();
+  array.forEach(likes => {
+    const target = document.getElementById(`like-${likes.item_id}`);
+    target.textContent = `Likes: ${likes.likes}`
+  });
 }
 
 
 export const postLike = async (id) => {
-fetch('https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/Q6FJ5Iv0xZsu9v3INtJx/likes', {
+  fetch('https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/Q6FJ5Iv0xZsu9v3INtJx/likes', {
     method: 'POST',
     // Adding body or contents to send
     body: JSON.stringify({
       item_id: id,
     }),
-
     // Adding headers to the request
     headers: {
       'Content-type': 'application/json; charset=UTF-8',
     },
   })
-
   .then((response) => response.json());
-
 }
