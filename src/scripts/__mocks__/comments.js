860096cce0f4d992f46a4f9383317c627ada24d6
Erez Friemagor <erezfree29@gmail.com>
2021-08-12 22:52:22 +0100

    test async function returns a promise

---
+++ src/scripts/__mocks__/comments.js
@@ -0,0 +1,10 @@
+
+async function fetchResultsJSON(itemId) {
+  itemId = itemId.replace(/\s/g, '%20');
+  const api = `https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/Q6FJ5Iv0xZsu9v3INtJx/comments?item_id=${itemId}`;
+  const response = await fetch(api);
+  const results = await response.json();
+  return results;
+}
+
+export default fetchResultsJSON;
