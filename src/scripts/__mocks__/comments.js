09b13522bbdb5417f6d57ea17ab95511de9a3292
Erez Friemagor <erezfree29@gmail.com>
2021-08-12 22:53:44 +0100

    align code

--- src/scripts/__mocks__/comments.js
+++ src/scripts/__mocks__/comments.js
@@ -1,4 +1,3 @@
-
 async function fetchResultsJSON(itemId) {
   itemId = itemId.replace(/\s/g, '%20');
   const api = `https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/Q6FJ5Iv0xZsu9v3INtJx/comments?item_id=${itemId}`;
